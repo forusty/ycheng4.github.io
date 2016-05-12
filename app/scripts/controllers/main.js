0f9d553414c43550ca8f32231dba6ebe9cb3a6b1
CYJ <ycheng4@e.ntu.edu.sg>
2016-05-12 22:37:05 +0800

    added in search feature, toggle between modes and also refresh function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,34 +6,92 @@
  * # MainCtrl
  * Controller of the wheresmytaxiApp
  */
-
 angular.module('wheresmytaxiApp')
-    .controller('MainCtrl', function(testService) {
+    .controller('MainCtrl', function($scope, testService) {
+        var map = '';
+        var heatmap = '';
+        $scope.currentMode = '';
         testService.queryData()
             .success(function(response) {
                 console.log(response);
-                var heatmapData = [];
-                angular.forEach(response.features[0].geometry.coordinates, function(value) {
-                    heatmapData.push(new google.maps.LatLng(value[1], value[0]));
-                });
-                console.log(heatmapData);
+                testService.setData(response);
+                $scope.currentMode = 'datapoint';
+
                 var mapDiv = document.getElementById('map');
-                var map = new google.maps.Map(mapDiv, {
+                map = new google.maps.Map(mapDiv, {
                     center: {
                         lat: 1.3521,
                         lng: 103.8198
                     },
                     zoom: 11
                 });
-                var heatmap = new google.maps.visualization.HeatmapLayer({
-                    data: heatmapData
-                });
-                heatmap.setMap(map);
+                map.data.addGeoJson(response);
             })
             .error(function(response, status) {
-                alert('Unable to load');
                 console.log(response);
                 console.log(status);
                 console.log(status);
             });
+        $scope.submit = function() {
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode({
+                'address': $scope.geoCode
+            }, function(results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location);
+                    map.setZoom(18);
+                } else {
+                    console.log('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        };
+        $scope.heatMap = function() {
+            $scope.currentMode = 'heatmap';
+            console.log();
+            if (typeof(heatmap)!=='string') {
+                heatmap.setMap(null);
+            }
+            map.data.forEach(function(feature) {
+                //If you want, check here for some constraints.
+                map.data.remove(feature);
+            });
+            //generate heat map data points into long,lat array
+            var heatmapData = [];
+            angular.forEach(testService.getData().features[0].geometry.coordinates, function(value) {
+                heatmapData.push(new google.maps.LatLng(value[1], value[0]));
+            });
+            //define the heat map layer from goolge api and set the data to the generated array of heatmap points
+            heatmap = new google.maps.visualization.HeatmapLayer({
+                data: heatmapData
+            });
+            heatmap.setMap(map);
+        };
+        $scope.dataPoint = function() {
+            $scope.currentMode = 'datapoint';
+            if (typeof(heatmap)!=='string') {
+                heatmap.setMap(null);
+            }
+            map.data.forEach(function(feature) {
+                //If you want, check here for some constraints.
+                map.data.remove(feature);
+            });
+            map.data.addGeoJson(testService.getData());
+        };
+        $scope.refresh = function() {
+            testService.queryData()
+                .success(function(response) {
+                    testService.setData(response);
+                    console.log(response);
+                    if ($scope.currentMode === 'heatmap') {
+                        $scope.heatMap();
+                    } else if ($scope.currentMode === 'datapoint') {
+                        $scope.dataPoint();
+                    }
+                })
+                .error(function(response, status) {
+                    console.log(response);
+                    console.log(status);
+                    console.log(status);
+                });
+        };
     });
--- app/scripts/services/service.js
+++ app/scripts/services/service.js
@@ -6,12 +6,13 @@ var jsonTest = angular.module('mapGeoJSONService', ['ngResource']);
 
 //FRC Sections Level 1 data services
 jsonTest.factory('testService', ['$resource', '$http',
-    function($resource,$http) {
+    function($resource, $http) {
         var config = {
             headers: {
-                'api-key':'x1EBGabWogDOPfCcEPXj1vp4XeHZ7lLh'
+                'api-key': 'x1EBGabWogDOPfCcEPXj1vp4XeHZ7lLh'
             }
         };
+        var data={};
         // var queryDataFunc = function() {
         //     var dataResource = $resource('json/abc.json', {}, {
         //         'query': {
@@ -25,7 +26,13 @@ jsonTest.factory('testService', ['$resource', '$http',
             return $http.get('https://api.data.gov.sg/v1/transport/taxi-availability', config);
         };
         return {
-            queryData: queryDataFunc
+            queryData: queryDataFunc,
+            setData: function(incomingData) {
+                data=incomingData;
+            },
+            getData: function() {
+                return data;
+            }
         };
     }
 ]);
